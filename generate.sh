62fba69c20c528dc0b7eb8a7334319e768aeae29
Frederico Silva <frederico.silva@gmail.com>
2019-06-13 17:10:45 +0200

    generate: always use latest generator

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
+docker pull openapitools/openapi-generator-cli
+
 docker run --rm \
     -v ${PWD}:/local openapitools/openapi-generator-cli generate \
     -i https://api.hyperone.com/openapi.json \
